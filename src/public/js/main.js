581416faac62f7e5311d8978a347a0dcdeadfa22
migohe14 <migohe14@gmail.com>
2018-10-26 20:51:55 +0200

    sockets

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,21 @@
-io();
+$(function() {
+    const socket = io();
+
+    //elements DOM
+
+    const $messageForm = $('#message-form');
+    const $messageBox = $('#message');
+    const $chat = $('#chat');
+
+    //events
+    $messageForm.submit(e => {
+        e.preventDefault()
+        socket.emit('send message', $messageBox.val());
+        $messageBox.val('');
+    })
+
+    socket.on('new message', function(data) {
+        $chat.append(data + '</br>');
+    })
+
+})
--- src/sockets.js
+++ src/sockets.js
@@ -1,5 +1,10 @@
 module.exports = function(io) {
     io.on('connection', socket => {
         console.log('user connected')
+
+        socket.on('send message', function(data) {
+            //servidor reenvía el dato a todos los sockets
+            io.sockets.emit('new message', data)
+        })
     });
 }
