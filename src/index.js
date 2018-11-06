57ae5c9b7fff2aaaafc3aa8dc38a61d357d9406e
migohe14 <migohe14@gmail.com>
2018-11-06 21:05:10 +0100

    mongodb

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
+const express = require('express');
+const socketio = require('socket.io');
+const mongoose = require('mongoose');
+
 const http = require('http');
 const path = require('path');
 
-const express = require('express');
-const socketio = require('socket.io');
-
+// initializing server and sockets
 const app = express();
-const server = http.createServer(app);
+const server= http.createServer(app);
 const io = socketio.listen(server);
 
-//settings
-app.set('port', process.env.PORT || 3000)
+// connection to the server
+mongoose.connect('mongodb://localhost/chat')
+  .then(db => console.log('db connected'))
+  .catch(err => console.log(err));
 
-require('./sockets')(io);
+// settings 
+app.set('port', process.env.PORT || 3000);
 
-//Send static files
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Starting the server
+// sockets
+require('./sockets')(io);
+
+// starting the server
 server.listen(app.get('port'), () => {
-    console.log('server on port ' + app.get('port'))
-})
+  console.log(`server on port ${app.get('port')}`);
+});
